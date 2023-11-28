8f4edcae37d110754d3ef138491735ea48139505
Dhruderg <94007244+Dhruderg@users.noreply.github.com>
2023-11-28 12:52:57 +0100

    Labor 8 - Rekursion added
    Fully finished Labor 8

--- Rekursion 7/Rekursion 7.c
+++ Rekursion 7/Rekursion 7.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
-int isPrime(int num) {
-
+int isPrime(int num, int start) {
+    //Prime num check, end of recursion
+    if (start == num) {
+        return 1;
+    }
+    //1 or 0, return 0, if divideable then 0
+    if (num % start == 0 || num <= 1) {
+        return 0;
+    }
+    return isPrime(num, start + 1); // Count index (start) upwards
 }
 
 int main(int argc, char* argv[]) {
+    int number;
+    scanf("%d", &number);
+    printf("%d", isPrime(number, 2));
     return 0;
 }
