dce4f63d522cb6b41d3dd136c41380949833f9f0
Dhruderg <94007244+Dhruderg@users.noreply.github.com>
2023-12-22 22:52:44 +0100

    Finished Labor

--- Strukturen 5/Strukturen 5.c
+++ Strukturen 5/Strukturen 5.c
@@ -1,45 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 typedef struct customer {
 	int ID;
-	char Vorname[];
-	char Nachname[];
+	char Vorname[20];
+	char Nachname[20];
 	struct customer *next;
 
 } customer;
 
-int* createCustomer(int ID, char Vorname[], char Nachname[])
-{
-	struct customer* newCustomer;
-	newCustomer->ID = ID;
-	strcpy(newCustomer->Vorname, Nachname);
-	strcpy(newCustomer->Nachname, Nachname);
-	newCustomer->next = NULL;
-	return newCustomer;
+void insert(struct customer* head) {
+	struct customer* current = head;	//Startpoint at head
+	struct customer* SortedList = NULL;	//create new linked list where elements sorted
 
-}
-
-void addCustomer(struct customer** head, struct customer* Customer)
-{
-	customer* newCustomer = (customer*)malloc(sizeof(customer));
-	if(*head == NULL)
+	while (current != NULL)
 	{
-		
+		struct customer* next = current->next;	// Next element in the list
+		if (SortedList == NULL || current->ID < SortedList->ID)	//If element == first in list or current ID smaller than sorted ID at same pos
+		{
+			current->next = SortedList;	//switch values
+			SortedList = current;		//switch values
+		} else
+		{
+			while(SortedList->next != NULL && SortedList->next->ID < current->ID) {	//While the value of next node is not null and its ID is smaller than current one's
+				SortedList = SortedList->next;	//switch temp
+			}
+			current->next = SortedList->next;	//synchronize pointers
+			SortedList->next = current;	// make next node the current node
+		}
+		current = next;	//Continue sorting from next element
 	}
 }
+customer* createCustomer(int ID, const char Vorname[],const char Nachname[])	//Create Node for customer
+{
+	customer* newCustomer = (customer*)malloc(sizeof(customer));
+	if (newCustomer==NULL){
+		exit(1);
+	}
+	/*Set the values*/
+	newCustomer->ID = ID;
+	strcpy(newCustomer->Vorname, Vorname);
+	strcpy(newCustomer->Nachname, Nachname);
+	newCustomer->next = NULL;
 
-void printList(struct customer* head)
+	return newCustomer;
+}
+
+void addCustomer(struct customer* head, struct customer* newCustomer)	//Add the new Customer into the List
+{
+	struct customer *current = head;
+	while (current->next != NULL){
+		current = current->next;
+	}
+	current->next = newCustomer;
+	insert(head);	//Sort the List
+}
+
+void printList(struct customer* head)	//Print the list by looping trough the nodes
 {
 	struct customer* current = head;
 	while (current != NULL) {
-		printf("%d: %s %s", current->ID, current->Vorname, current->Nachname);
+		printf("%d: %s %s\n", current->ID, current->Vorname, current->Nachname);
 		current = current->next;
 	}
 }
 
-void clearList(struct customer * head)
+void clearList(struct customer * head)	//Clear every single node
 {
 	struct customer * current = head;
 	struct customer* next;
@@ -50,9 +78,16 @@ void clearList(struct customer * head)
 	}
 }
 
-void customerCount()
+int customerCount(struct customer * head)	//Gives back how many nodes there is in the linked list
 {
-
+	struct customer* current = head;
+	int count = 0;
+	while (current!=NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	return count;
 }
 
 int main(int argc, char* argv[]) {
@@ -60,6 +95,10 @@ int main(int argc, char* argv[]) {
 	root = createCustomer(1, "Hans", "Maurer");
 	addCustomer(root, createCustomer(3, "Tatjana", "Roth"));
 	addCustomer(root, createCustomer(2, "Anna-Maria", "Schmidt"));
+	addCustomer(root, createCustomer(4, "Anna-Maria4", "Schmidt"));
+	addCustomer(root, createCustomer(5, "Anna-Maria5", "Schmidt"));
+	addCustomer(root, createCustomer(10, "Anna-Maria", "Schmidt"));
+	addCustomer(root, createCustomer(7, "Anna-Maria", "Schmidt"));
 	printf("Momentan sind %u Kunden erfasst.\n", customerCount(root));
 	printList(root);
 	clearList(root);
@@ -69,4 +108,3 @@ int main(int argc, char* argv[]) {
 	printList(root);
 	return 0;
 }
-
